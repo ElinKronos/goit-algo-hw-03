f81ed086ec7bc7df5b10d632ac036527cdeb6c83
ElinKronos <elinder2010@gmail.com>
2025-05-30 21:09:32 +0300

    1 and 2 tasks done

--- 1_First_task.py
+++ 1_First_task.py
@@ -1,3 +1,95 @@
+# Here begins a new world...
+
+from datetime import datetime
+
+def get_days_from_today(date):
+    
+    """
+    Обчислює кількість днів між поточною датою та заданою датою.
+
+    Параметри:
+    - date_str (str): дата у форматі 'YYYY-MM-DD'
+
+    Повертає:
+    - int: кількість днів між сьогоднішньою датою та заданою
+    """
+
+    today = datetime.now().date()
+    my_date = string_to_date(date)
+
+    days = (today - my_date).days
+
+    return days
+
+def input_date():
+
+    """
+    Запитує у користувача дату (рік, місяць, день), перевіряє її на валідність 
+    та підтвердження. Повертає рядок дати у форматі 'YYYY-MM-DD'.
+
+    Повертає:
+    - str: коректно введена та підтверджена дата у форматі 'YYYY-MM-DD'
+    """
+
+    while True:
+        print("Введіть свою дату")
+        try:
+            # Перевіряємо правильність введення року
+            while True:
+                y = int(input("Введіть рік (у форматі РРРР): "))
+                if y > 0:
+                    break
+                else:
+                    print("Рік не можу бути від'ємним. Введіть правильне значення")
+                    continue
+            
+            # Перевіряємо правильність введення місяця
+            while True:
+                m = int(input("Введіть місяць (у форматі ММ): "))
+                if 1 <= m <= 12:
+                    break
+                else:
+                    print("Такого місяця не існує, їх усього 12. Тому введи правильне значення.")
+                    continue
+            
+            # Перевіряємо правильність введення дня
+            while True:
+                d = int(input("Введіть день (у форматі ДД): "))
+                if 1 <= d <= 31:
+                    break
+                else:
+                    print("В місяці максимум 31 день, тому введіть правильне число.")
+                    continue
+
+        except ValueError:
+            print("Ви ввели неправильне значення. Введіть цифрові значення")
+            continue
+
+        print(f"Ви ввели: {y:04d}-{m:02d}-{d:02d} !\nЦе правильно?\n(1 - Так; 2 - Ні)")
+        
+        # Перевірка та підтвердження введення правильності дати
+        while True:
+            try:
+                choice = int(input())
+                if choice == 1:
+                    date = str(f"{y:04d}-{m:02d}-{d:02d}")
+                    return date
+                elif choice == 2:
+                    print("Повернення на початок.")
+                    break
+                else:
+                    print("Ви ввели невірне значення.\nПотрібно ввести: 1 - Так; 2 - Ні")
+                    continue
+            except ValueError:
+                print("Ви ввели невірне значення, має бути число.")
+        
+
+def string_to_date(date_string):
+    return datetime.strptime(date_string, "%Y-%m-%d").date()
+
+# Запуск програми
+print(get_days_from_today(input_date()))
+
 
 
 
---
+++ 2_Second_Task.py
@@ -0,0 +1,39 @@
+# Here begins a new world...
+
+import random
+
+def get_number_tickets(min, max, quantity):
+
+    """
+    Генерує відсортований список унікальних випадкових чисел.
+
+    Аргументи:
+    - min (int): найменше можливе число (діапазон початку)
+    - max (int): найбільше можливе число (діапазон кінця)
+    - quantity (int): скільки чисел потрібно обрати
+
+    Повертає:
+    - list[int]: Відсортований список унікальних чисел, якщо параметри валідні
+    - list[]: Порожній список, якщо параметри не відповідають вимогам
+    """
+
+    # Перевірка валідності введених даних
+    if min < 1 or max > 1000:
+        print("Межі діапазону мають бути в межах від 1 до 1000.")
+        return []
+    
+    if quantity < min or quantity > max:
+        print(f"Кількість чисел має бути в діапазоні від {min} до {max}.")
+        return []
+    
+    # Створюємо робочий список чисел
+    work_space = list(range(min, max +1))
+
+    # Обираємо випадкові унікальні числа без повторів
+    new_list = random.sample(work_space, quantity)
+
+    return sorted(new_list)
+
+# Викликаємо функцію
+numbers = get_number_tickets(1, 49, 8)
+print("Ваші лотерейні номери:", numbers)
